786ace04065e10d953c9a3b62b4b26e56a00436b
Daniel Correia <devdanielcorreia@gmail.com>
2024-03-18 19:21:09 -0300

    first commit

---
+++ src/main/java/com/m1s9/exerciciosemana9/Exerciciosemana9Application.java
@@ -0,0 +1,13 @@
+package com.m1s9.exerciciosemana9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exerciciosemana9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exerciciosemana9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/m1s9/exerciciosemana9/Exerciciosemana9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m1s9.exerciciosemana9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exerciciosemana9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
